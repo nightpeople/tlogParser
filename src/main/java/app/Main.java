b7ccd352c238637ced3384cdbdd2c38aee9ec020
nightpeople <982139717@163.com>
2023-01-06 22:25:44 +0800

    根据tlog.xml向mysql库加表/更新表结构

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,28 +5,34 @@ import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Properties;
 
 import app.module.DBLoader;
 import app.module.XMLParser;
 
 public class Main {
-    public static void main(String[] args) throws DocumentException, MalformedURLException {
+    public static void main(String[] args) throws DocumentException, IOException {
         // TODO args参数, tlog路径等
         // 取jar包同级tlog.xml文件
         String jarPath = new File("").getAbsolutePath();
         String tlogPath;
+        String configPath; //server.config文件
         URL resource;
         Document document;
         SAXReader saxReader = new SAXReader();
         if (File.separatorChar == jarPath.charAt(jarPath.length() - 1)) {
             tlogPath = jarPath + "tlog.xml";
+            configPath = jarPath + "server.config";
         } else {
             tlogPath = jarPath + File.separatorChar + "tlog.xml";
+            configPath = jarPath + File.separatorChar + "server.config";
         }
 
         File tlogFile = new File(tlogPath);
+        File configFile = new File(configPath);
         // 找不到就取内部的tlog.xml
         if (!tlogFile.exists()) {
             tlogPath = "/tlog.xml";
@@ -36,8 +42,22 @@ public class Main {
             document = saxReader.read(tlogFile);
         }
 
+        // 找不到就取内部的server.config
+        Properties properties = new Properties();
+        if (!configFile.exists()) {
+            configPath = "/server.config";
+            properties.load(Main.class.getResourceAsStream(configPath));
+        } else {
+            properties.load(new FileReader(configFile));
+        }
+
+        //        System.out.println(properties.getProperty("db_url"));
+        //        System.out.println(properties.getProperty("db_user"));
+        //        System.out.println(properties.getProperty("db_passwd"));
+        //        System.exit(0);
+
         XMLParser XMLParser = new XMLParser(document);
-        DBLoader dbLoader = new DBLoader();
+        DBLoader dbLoader = new DBLoader(properties);
         XMLParser.parse();
 
     }
--- src/main/java/app/module/DBLoader.java
+++ src/main/java/app/module/DBLoader.java
@@ -1,10 +1,15 @@
 package app.module;
 
+import java.util.Properties;
+
 /**
  * 加载mysql库所有表信息
  */
 public class DBLoader {
 
-    public DBLoader() {
+    public Properties properties;
+
+    public DBLoader(Properties properties) {
+        this.properties = properties;
     }
 }
